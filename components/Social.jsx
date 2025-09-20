64cb68c6a1c857f498e7b7d33ecda14c4d4087b0
ali <alijafar.rajani@gmail.com>
2025-09-20 10:54:52 +0500

    LeetCode Link Added

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 
-import {FaGithub, FaLinkedinIn, FaTwitter,} from 'react-icons/fa'
-import { SiGmail } from "react-icons/si";
+import {FaGithub, FaLinkedinIn, FaTwitter} from 'react-icons/fa'
+import { SiGmail, SiLeetcode } from "react-icons/si";
 
 
 const socials = [
     { icon: <FaGithub />, link: "https://github.com/s1lent18" },
     { icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/ali-jafar" },
     { icon: <FaTwitter />, link: "https://x.com/s1lent_12" },
-    { icon: <SiGmail />, link: "mailto:alijafar.rajani@gmail.com" }
+    { icon: <SiGmail />, link: "mailto:alijafar.rajani@gmail.com" },
+    { icon: <SiLeetcode />, link: "https://leetcode.com/u/S1lent18/"}
 ];
 
 const Social = ({containerStyles, iconStyles}) => {
