93da08052cafd14ae74da9128cbabf914049fae8
ali <alijafar.rajani@gmail.com>
2026-03-15 17:47:54 +0500

    Experience Added

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -257,11 +257,11 @@ const Resume = () => {
                         </TabsContent>
                         <TabsContent value="experience" className="w-full">
                             <div className="flex flex-col gap-[30px] text-center xl:text-left">
-                                <h3 className="text-4xl font-bold">{education.title}</h3>
-                                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{education.description}</p>
+                                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experience.description}</p>
                                 <ScrollArea className="h-[400px]">
                                     <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
-                                        {education.item.map((item, index) => {
+                                        {experience.item.map((item, index) => {
                                             return (
                                                 <li
                                                     key={index}
